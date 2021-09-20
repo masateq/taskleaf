fd8ac375db781637bffbefc1304e2cbfadc6bc8a
masateq <masaya0821s@gmail.com>
2021-09-20 14:56:02 +0900

    Complete taskleaf

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -11,6 +12,19 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました。"
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
   end
 
   def create
@@ -25,3 +39,4 @@ class TasksController < ApplicationController
     params.require(:task).permit(:name, :description)
   end
 end
+
